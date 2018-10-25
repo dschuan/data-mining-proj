8f1a86980f771ce7275f6d9779287393b682122d
nathanaelraj <nathanaelraj@hotmail.com>
2018-10-24 17:49:33 -0700

    added function in svm

--- svm.py
+++ svm.py
@@ -10,18 +10,19 @@ from pylab import plt
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 
-data = ic.separateImport()
-data = procd.fillData(data, fill_method="median")
+def gridSearchSVM():
+    data = ic.separateImport()
+    data = procd.fillData(data, fill_method="median")
 
-testX, testY, trainX, trainY = procd.createTrainingSet(data)
+    testX, testY, trainX, trainY = procd.createTrainingSet(data)
 
-svc = SVC()
-param_grid = [
-  {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-  {'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 'kernel': ['rbf', 'sigmoid']},
-  {'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 'degree':[2, 3, 4], 'kernel': ['poly']}
-]
-gs = GridSearchCV(svc, param_grid, verbose=2)
-gs.fit(trainX, trainY)
-score = gs.score(testX,testY)
-print(score)
+    svc = SVC()
+    param_grid = [
+      {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+      {'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 'kernel': ['rbf', 'sigmoid']},
+      {'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 'degree':[2, 3, 4], 'kernel': ['poly']}
+    ]
+    gs = GridSearchCV(svc, param_grid, verbose=2)
+    gs.fit(trainX, trainY)
+    score = gs.score(testX,testY)
+    return score
